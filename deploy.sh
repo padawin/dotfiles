b6a97a622e09324ebf0660a45f6e8fe0e427e729
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-08-04 12:21:23 +0200

    deployment script created, to use those files in a separated folder... It avoids to have the .git in ~, cleaner

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+SOURCE=$(dirname $(readlink -f $0))
+
+files[0]='.gconf/apps/gnome-terminal'
+files[1]='.ackrc'
+files[2]='.bashrc'
+files[3]='.config/awesome'
+files[4]='.config/i3status'
+files[5]='.config/geany'
+files[6]='.scripts'
+files[7]='.vimrc'
+files[8]='.bash_aliases'
+files[9]='.gitconfig'
+files[10]='.vim'
+files[11]='.i3'
+
+for f in "${files[@]}"
+do
+	rm -rf $HOME/$f
+	ln -s $SOURCE/$f $HOME/$f
+done
+
