ab3d28c6b99cb67597603380efd88424d8fb430f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-07-25 15:57:02 +0100

    bash_profile added

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,12 @@ files[4]='.config/i3status'
 files[5]='.config/geany'
 files[6]='.scripts'
 files[7]='.vimrc'
-files[8]='.bash_aliases'
-files[9]='.gitconfig'
-files[10]='.vim'
-files[11]='.i3'
-files[12]='.tmux.conf'
+files[8]='.bash_profile'
+files[9]='.bash_aliases'
+files[10]='.gitconfig'
+files[11]='.vim'
+files[12]='.i3'
+files[13]='.tmux.conf'
 
 for f in "${files[@]}"
 do
